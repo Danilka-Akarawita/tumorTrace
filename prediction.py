b7914da83fa7d3561b3a8149410529d118c3c6c1
YasithaRenuk <126389901+YasithaRenuk@users.noreply.github.com>
2025-05-04 18:29:02 +0530

    add notebook

--- prediction.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def predict_image(model, img_array):
-    predictions = model.predict(img_array)
-    predicted_class_index = np.argmax(predictions, axis=1)[0]
-    return predicted_class_index, predictions
--- training.py
+++
@@ -1,3 +0,0 @@
-def train_model(model, train_data, valid_data, epochs=5):
-    history = model.fit(train_data, epochs=epochs, validation_data=valid_data, shuffle=False)
-    return history
