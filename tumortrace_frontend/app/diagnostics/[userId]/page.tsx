e444e0a3dd87245c9fc1566cce86a8db0b7bac1f
YasithaRenuk <126389901+YasithaRenuk@users.noreply.github.com>
2025-05-04 18:27:23 +0530

    i don't remember what i did here

--- tumortrace_frontend/app/diagnostics/[userId]/page.tsx
+++ tumortrace_frontend/app/diagnostics/[userId]/page.tsx
@@ -1,11 +1,57 @@
-// app/posts/[userId]/page.tsx
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
 export default function PostPage({ params }: { params: { userId: string } }) {
   const { userId } = params;
+  const [hasImg, setHasImg] = useState<boolean>(false);
+  const [hasImg2, setHasImg2] = useState<boolean>(false);
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    setHasImg(true);
+  };
+
+  const handleSubmit2 = (event: React.FormEvent) => {
+    event.preventDefault();
+    setHasImg2(true);
+  };
 
   return (
-    <div>
-      <h1>Post userId: {userId}</h1>
-      {/* Your component logic here */}
+    <div className="min-h-screen bg-gray-100 py-8">
+      <div className="container mx-auto max-w-2xl space-y-8">
+        <h1>Patient Id: {userId}</h1>
+        <div className="bg-white p-6 rounded-lg shadow-md">
+          <Button onClick={handleSubmit} className="w-full">
+            Generate Diagnostics
+          </Button>
+        </div>
+        <div className="bg-white p-6 rounded-lg shadow-md">
+          {hasImg && (
+            <div className="mt-4">
+              <h1>Diagnostics:</h1>
+              <h1 className="text-5xl text-red-600">glioma</h1>
+            </div>
+          )}
+        </div>
+        <div className="bg-white p-6 rounded-lg shadow-md">
+          <Button onClick={handleSubmit2} className="w-full">
+            Generate MRI
+          </Button>
+        </div>
+        <div className="bg-white p-6 rounded-lg shadow-md">
+          <div>
+            {hasImg2 && (
+              <img
+                src="/glioma-8-seg.PNG"
+                alt="Uploaded MRI"
+                className="w-full h-auto rounded-lg"
+              />
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
